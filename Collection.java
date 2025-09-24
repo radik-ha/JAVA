46d1af5f3ff8e85bb6e2a66a0d9cbef8b3132ed1
Radhika <rathikaari1@gmail.com>
2025-09-24 11:35:15 -0700

    Add files via upload

---
+++ Collection.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class Collection
+{
+public static void main(String args[])
+{
+Stack <Integer> s = new Stack <Integer> ();
+s.push(10);
+s.push(30);
+s.push(60);
+System.out.println(s);
+s.pop();
+System.out.println(s);
+}
+}
